6c3cc49a6ad1d5b93f50091b696d297f925b2fb8
yucheon6000 <yucheon6000@gmail.com>
2024-04-24 10:45:07 +0900

    Add traffic light model

---
+++ Assets/Scripts/Environment/Traffic Light/TrafficLight.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLightState { }
+
+public class TrafficLight : MonoBehaviour
+{
+}
+
+[Serializable]
+public class TrafficLightInfo
+{
+}
