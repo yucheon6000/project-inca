9f8efd05ee6f63d5c9790621e4b2949cc93f1cb6
yucheon6000 <yucheon6000@gmail.com>
2024-08-27 17:27:52 +0900

    Apply the morning glory enemy model and animation

--- Assets/3 Scripts/4 Game/Enemy/Enemy_MorningGlory_Speaker.cs
+++ Assets/3 Scripts/4 Game/Enemy/Enemy_MorningGlory_Speaker.cs
@@ -9,6 +9,24 @@ public class Enemy_MorningGlory_Speaker : DamagableEnemy
     {
         transform.localScale = Vector3.one * Random.Range(0.4f, 0.7f);
         scaleEffector.SaveOriginalScale(true);
+        LookAtPlayer(true);
         base.Init(detectedObject);
+    }
+
+    public void ForceKill(float time)
+    {
+        Invoke(nameof(ForceKill), time);
+    }
+
+    public void PlayTakeDamageEffect(float time)
+    {
+        Invoke(nameof(PlayTakeDamageEffect_), time);
+    }
+
+    private void PlayTakeDamageEffect_()
+    {
+        if (gameObject.activeSelf == false) return;
+
+        emissionEffector.Play(0.4f, 10f, 0f, Color.red);
     }
 }
